c8108faa2ce89f549e23befbc71ca2784ef21112
masabon0306fanta-commits <masabon0306fanta@gmail.com>
2026-05-12 15:50:55 +0900

    Delete Parking directory

--- Parking/src/main/java/com/example/demo/Repository/ParkingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.entity.Parking;
-
-public interface ParkingRepository extends JpaRepository <Parking, Integer>{
-
-}
